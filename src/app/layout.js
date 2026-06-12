18eee8e4efb7af018fdcbd5c0b3dd07a6451fd5c
KOUMAD Abdelahkim <217803801+HakimDev-tech@users.noreply.github.com>
2026-06-12 10:02:41 +0100

    Remove Analytics component from layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from '@vercel/analytics/react';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +31,6 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Analytics />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
